46931b7fc69e6433856ed4ac32bc4e23d042fd94
Reinaldo Barbosa <reinaldobarbosa034@gmail.com>
2023-06-20 11:04:28 -0300

    logica parcial para o cadastro de produtos

--- src/main/java/com/itb/inf2em/comercio/model/Produto.java
+++ src/main/java/com/itb/inf2em/comercio/model/Produto.java
@@ -3,7 +3,7 @@ package com.itb.inf2em.comercio.model;
 public class Produto {
 	private  Long id;
 	private String nome;
-	private String decricao;
+	private String descricao;
 	private String codigoBarras;
 	private double preco;
 	
@@ -15,7 +15,52 @@ public class Produto {
 	
 	// Metodo Setter's - Atribuiu dados aos atributos
 	
-	public void satId (Long id) {
-		this.id = id/
+	//Id
+	
+	public void setId (Long id) {
+		this.id = id;
+	}
+	
+	public Long getId () {
+		return id;
+	}
+	
+	//Nome
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getNome () {
+		return nome;
+	}
+	
+	//Descrição
+	
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	public String getDescricao () {
+		return descricao; 
+	}
+	
+	//Codigo de Barras
+	
+	public void setCodigoDeBarras(String codigoBarras) {
+		this.codigoBarras = codigoBarras;
+	}
+	public String getCodigoDeBarras () {
+		return codigoBarras;
+	}
+	
+	//Preço
+	
+	public void setPreco (double preco) {
+		this.preco = preco;
+	}
+	
+	public double getPreco () {
+		return preco; 
 	}
 }
